5037213cec28f5b92ee3d6a8a6b535b7680971b5
Kostya <tkach.black@gmail.com>
2025-04-13 03:04:18 +0300

    Delete src directory

--- src/scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-// This file is intentionally left blank.
-document.addEventListener('DOMContentLoaded', () => {
-    const portfolioSection = document.getElementById('portfolio');
-    const newProject = document.createElement('p');
-    newProject.textContent = 'Проект на Python: Анализ данных';
-    portfolioSection.appendChild(newProject);
-});
